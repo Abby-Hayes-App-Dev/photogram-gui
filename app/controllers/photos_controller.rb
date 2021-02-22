dcb3594ed8be7a54a0d6e7c2a7b805265697e79c
abbyehayes <abbyehayes@uchicago.edu>
2021-02-22 01:06:07 +0000

    57:09 in the video

--- app/controllers/photos_controller.rb
+++ app/controllers/photos_controller.rb
@@ -1,5 +1,7 @@
 class PhotosController < ApplicationController
   def index
+    matching_photos = 
+    # @list_of_photos = 
     render({ :template => "photo_templates/index.html.erb"})
   end
 end
